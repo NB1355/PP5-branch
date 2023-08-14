2743f6c339d95e837cc96afb1a6e8ec31c38b8ab
NB1355 <nahal5513@gmail.com>
2023-08-14 20:33:45 +0000

    ADD create shopping bag functioality base

--- veriwiseweb/settings.py
+++ veriwiseweb/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     # 'home',
     'pages',
-    'products'
+    'products',
+    'bag',
 ]
 
 MIDDLEWARE = [
--- veriwiseweb/urls.py
+++ veriwiseweb/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     # path('', include('home.urls')),
     path('', include('pages.urls')),
-    path('products/', include('products.urls'))
+    path('products/', include('products.urls')),
+    path('', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
